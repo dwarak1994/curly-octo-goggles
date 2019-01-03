f76683428b8a801b0aad63b9798b11125441b64c
dwarak1994 <40156858+dwarak1994@users.noreply.github.com>
2019-01-02 22:35:17 -0500

    Update RestEx.py

--- RestEx.py
+++ RestEx.py
@@ -4,11 +4,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def hello():
-    if request.method=='POST':
-        sone_json=request.get_json()
-        return jsonify({"you sent":sone_json}), 201
-    else:
-        return jsonify({"about":"hello world"})
+    return "HELLO WORLD"
 
 if __name__=='__main__':
     app.run(debug=True)
